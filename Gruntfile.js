3ba1b04d9b34bf37590a67e01d9d817d1933596a
Baptiste Régné <baptiste.regne@yahoo.fr>
2016-01-14 16:45:00 +0100

    Blank Wordpress Ready Theme

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        watch: {
+
+            sass: {
+                files: ['scss/**/*.{scss,sass}'],
+                tasks: ['sass:dist', 'rsync'],
+            },
+
+            js : {
+                files: ['js/**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true,
+                    livereloadOnError: false,
+                    spawn: false
+                }
+            },
+
+            other: {
+                files: ['**/*.php', 'css/*.css'],
+                options: {
+                    livereload: true,
+                    livereloadOnError: false,
+                    spawn: false
+                }
+            }
+        },
+
+        jshint: {
+            all: ['js/**/*.js', '!js/foundation/**/*.js', '!js/vendor/**/*.js']
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                }
+            },
+            options: {
+                compass: true,
+                //quiet: true,
+                style: 'nested',
+                require: [ 'susy', 'font-awesome-sass']
+            }
+        },
+
+        rsync: {
+            options: {
+                // args: ["-q"],
+                //exclude: [".git*","assets/less","node_modules"],
+                recursive: true
+            },
+            dist: {
+                options: {
+                    // src: ["./assets/css/dw-timeline-pro-flat.min.css", "./assets/css/dw-timeline-pro-flat.min.css.map"],
+                    // dest: "/var/www/html/laventurierviking/wp-content/themes/dw-timeline-pro/assets/css/",
+
+                    src: "./css",
+                    dest: "/var/www/html/client/oniris/wp-content/themes/oniris-v2",
+                    host: "laventurier@onlinet",
+                }
+            }
+        },
+
+    });
+
+    grunt.registerTask('default', ['watch']);
+};
